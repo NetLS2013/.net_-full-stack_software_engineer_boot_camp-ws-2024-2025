26df6937dc4b05a4cd248f46853006326911a85b
Rostyk232003 <rostislavkopko97@gmail.com>
2026-01-02 21:28:56 +0200

    Add project files.

---
+++ OrdersLibrary/Order.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OrdersLibrary
+{
+    public class OrderBase
+    {
+        public int OrderNumber { get; set; }
+
+        protected decimal DiscountPercent { get; set; }
+
+        private protected decimal InternalDiscount { get; set; }
+
+        public OrderBase(int orderNumber)
+        {
+            OrderNumber = orderNumber;
+            DiscountPercent = 10;
+            InternalDiscount = 5;
+        }
+    }
+
+    public class Order : OrderBase
+    {
+        public Order(int orderNumber) : base(orderNumber) {}
+
+        public void ShowDiscounts()
+        {
+            Console.WriteLine($"Discount: {DiscountPercent}%");
+            Console.WriteLine($"Internal Discount: {InternalDiscount}%");
+        }
+    }
+
+    public class ItemBase
+    {
+        public string ItemName { get; set; }
+
+        protected decimal WholesalePrice { get; set; }
+
+        private protected decimal InternalCost { get; set; }
+
+        public ItemBase(string name)
+        {
+            ItemName = name;
+            WholesalePrice = 100;
+            InternalCost = 80;
+        }
+    }
+
+    public class Item : ItemBase
+    {
+        public Item(string name) : base(name) { }
+
+        public void ShowPrices()
+        {
+            Console.WriteLine($"Wholesale price: {WholesalePrice}");
+            Console.WriteLine($"Internal cost: {InternalCost}");
+        }
+    }
+}
---
+++ ShopApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UsersLibrary;
+using OrdersLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Customer customer = new Customer(101, "Іван", "ivan@mail.com");
+        customer.ShowPrivateData();
+
+        Order order = new Order(555);
+        order.ShowDiscounts();
+
+        Item item = new Item("Ноутбук");
+        item.ShowPrices();
+    }
+}
+
