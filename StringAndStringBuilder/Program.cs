21e2b5f423110537110cef97448dc2f57a0b64e0
Petro Shchesniuk <pshehesnyk4@gmail.com>
2026-01-03 13:13:49 +0200

    Added String and String Builder Task

---
+++ StringAndStringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+namespace StringAndStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            var iterations = 50000;
+            sw.Restart();
+            string baseString = "Hello, World!";
+            for (int i = 0; i < iterations; i++)
+            {
+                baseString += " Adding more text.";
+            }
+            sw.Stop();
+            Console.WriteLine($"String: {sw.ElapsedMilliseconds} ms");
+            
+            sw.Restart();
+            var sb = new System.Text.StringBuilder("Hello, World!");
+            for (int i = 0; i < iterations; i++)
+            {
+                sb.Append(" Adding more text.");
+            }
+            sw.Stop();
+            Console.WriteLine($"StringBuilder: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
