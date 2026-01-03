2d06367de38b5617c5e4d1d0b43bbde9f3f93ec8
Petro Shchesniuk <pshehesnyk4@gmail.com>
2026-01-03 15:04:47 +0200

    Added Generics Task

---
+++ Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Generics
+{
+    public abstract class GenericTestClassBase<T>
+    {
+        public T Params { get; set; }
+
+        public abstract bool Equals(T param);
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public bool isWeath { get; set; }
+        
+    }
+
+    public class PersonTest : GenericTestClassBase<Person>
+    {   public override bool Equals(Person param)
+        {
+            return Params.name == param.name &&
+               Params.age == param.age &&
+               Params.isWeath == param.isWeath;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonTest personTest = new PersonTest();
+            personTest.Params = new Person { name = "John", age = 30, isWeath = true };
+
+            Person personToCompare = new Person { name = "John", age = 30, isWeath = true };
+            bool areEqual = personTest.Equals(personToCompare);
+            Console.WriteLine($"Are the two persons equal? {areEqual}");
+
+            Person anotherPersonToCompare = new Person { name = "Jane", age = 25, isWeath = false };
+            bool areAnotherEqual = personTest.Equals(anotherPersonToCompare);
+            Console.WriteLine($"Are the two persons equal? {areAnotherEqual}");
+        }
+    }
+}
