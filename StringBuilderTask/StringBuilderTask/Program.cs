05ae1f0e8618e96dd3f5b024a8eac9bca1e3d14d
VladHolobyn <vlad.holobyn@gmail.com>
2026-01-11 14:56:13 +0200

    String VS StringBuilder task

---
+++ StringBuilderTask/StringBuilderTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Stopwatch sw = new Stopwatch();
+        int iterations = 150_000;
+        string textToAdd = "abc";    
+
+
+
+        Console.WriteLine($"Benchmark with {iterations:N0} iterations...\n");
+        // ---------------------------------------------------------
+        // 1. Звичайний String
+        // ---------------------------------------------------------
+        string str = "";
+
+        sw.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            str += textToAdd;
+        }
+        sw.Stop();
+        Console.WriteLine($"String (+): \t\t{sw.ElapsedMilliseconds} ms");
+
+
+
+        // ---------------------------------------------------------
+        // 2. StringBuilder
+        // ---------------------------------------------------------
+        StringBuilder sb = new StringBuilder();
+
+        sw.Restart();
+        for (int i = 0; i < iterations; i++)
+        {
+            sb.Append(textToAdd);
+        }
+        sw.Stop();
+        Console.WriteLine($"StringBuilder: \t\t{sw.ElapsedMilliseconds} ms");
+
+    }
+
+}
